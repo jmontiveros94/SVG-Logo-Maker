e7ecf6fda9d42ae58d63085b67cb61339d179588
Jacob <jm.ontiveros94@gmail.com>
2023-11-10 15:45:11 -0500

    minor tweaks to test.js

--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -3,13 +3,13 @@ const { Triangle, Circle, Square } = require('./shapes');
 describe('Shape', () => {
     describe ('Circle', () => {
         test ('should return the SVG file for circles', () => {
-            const circle = new Circle('green');
+            const circle = new Circle('red');
             expect(circle.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="red" />`);
         });
     });
     describe ('Square', () => {
         test ('should return the SVG code for a square', () => {
-            const square = new Square('green');
+            const square = new Square('blue');
             expect(square.render()).toEqual(`<rect x="70" y="20" width="160" height="160" fill="blue" />`);
         });
     });
